fca56dc2ab6f4b8031f7c63bffc416d37e44e2ab
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-25 16:31:31 -0300

    refactor: refatoracao nos codigos para jpa

--- src/main/java/com/gm2/crypto/controller/CoinController.java
+++ src/main/java/com/gm2/crypto/controller/CoinController.java
@@ -3,12 +3,10 @@ package com.gm2.crypto.controller;
 import com.gm2.crypto.entity.Coin;
 import com.gm2.crypto.repository.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 @RestController
@@ -56,10 +54,12 @@ public class CoinController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable int id){
+        boolean response = false;
         try {
-            return new ResponseEntity<>(coinRepository.remove(id), HttpStatus.OK);
+            response = coinRepository.remove(id);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }catch (Exception exception){
-            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
         }
     }
 }
