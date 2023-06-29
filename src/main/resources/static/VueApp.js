287e4f6b0f181ea639a7a39c9775f1b7e4222e9e
IgorMontezuma20 <igorminranda17@gmail.com>
2023-06-29 12:46:07 -0300

    feat: cadastrando novas moedas com axios

--- src/main/resources/static/VueApp.js
+++ src/main/resources/static/VueApp.js
@@ -1,14 +1,14 @@
-const { createApp } = Vue;
+const {createApp} = Vue;
 
 const baseUrl = "http://localhost:8080/coin"
 
 const mainContainer = {
-    data(){
+    data() {
         return {
-            coins:[],
+            coins: [],
             formCoin: {
                 isNew: true,
-                name : '',
+                name: '',
                 price: '',
                 quantity: '',
                 title: 'Cadastrar nova transação',
@@ -16,11 +16,11 @@ const mainContainer = {
             }
         }
     },
-    mounted(){
+    mounted() {
         this.showAllCoins()
     },
     methods: {
-        showAllCoins(){
+        showAllCoins() {
             axios
                 .get(baseUrl)
                 .then(response => {
@@ -28,6 +28,47 @@ const mainContainer = {
                         this.coins.push(item)
                     })
                 })
+        },
+
+        saveCoin() {
+            this.coins = []
+            this.formCoin.name = this.formCoin.name.toUpperCase()
+            this.formCoin.price = this.formCoin.price.replace("R$", "")
+                .replace(",", ".").trim()
+
+            if (this.formCoin.name == '' || this.formCoin.price == '' || this.formCoin.quantity == '') {
+                toastr.error('É preciso preencher todos os campos!', 'Formulário')
+                return
+            }
+
+            const coin = {
+                name: this.formCoin.name,
+                price: this.formCoin.price,
+                quantity: this.formCoin.quantity
+            }
+
+            const self = this
+
+            axios.post(baseUrl, coin)
+                .then(function (response) {
+                    toastr.success('Transação cadastrada com sucesso!', 'Formulário')
+                })
+                .catch(function (error) {
+                    toastr.error('Não foi possível cadastrar uma nova transação', 'Formulário')
+                })
+                .then(function () {
+                    self.showAllCoins()
+                    self.cleanForm()
+                })
+        },
+
+        cleanForm() {
+            this.formCoin.isNew = true
+            this.formCoin.name = ''
+            this.formCoin.price = ''
+            this.formCoin.quantity = ''
+            this.formCoin.title = 'Cadastrar nova transação'
+            this.formCoin.button = 'Cadastrar'
         }
     }
 }
